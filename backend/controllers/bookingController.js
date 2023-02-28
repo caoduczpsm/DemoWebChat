1f10f426c9a25740524744034c60f8a0ce68d3e0
ncaoducgit-test <nguyencaoduc0922@gmail.com>
2023-02-28 17:03:40 +0700

    block history comment console log

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -11,13 +11,13 @@ const app = express();
 checkUserInBlocklist = asyncHandler(async (req, res, next) => {
 
     const userEmail = localStorage.getItem('userInfo');
-    console.log(userEmail);
+    //console.log(userEmail);
     const blocklist = await BlockList.get({ "email": userEmail });
-    console.log(blocklist);
+    //console.log(blocklist);
     const blockHistoryId = blocklist.blockhistory;
-    console.log(blockHistoryId);
+    //console.log(blockHistoryId);
     const blockHistory = await BlockHistory.get({ "_id": blockHistoryId });
-    console.log(blockHistory);
+    //console.log(blockHistory);
     if (blocklist && (blockHistory.blockstate === "BLOCKED")) {
         res.status(403);
         res.json({ message: "Please contact to admin system to book this rom!" });
